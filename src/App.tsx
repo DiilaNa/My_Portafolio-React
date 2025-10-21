7ac174cd28db38718125f43d9301dd9b5aee3963
DiilaNa <liyanaarachchidilan@gmail.com>
2025-10-21 13:54:47 +0530

    add a page to test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import NavBar from './components/NavBar'
+import Home from './Sections/Home'
 
 const App:React.FC=() => {
 
   return (
     <>
         <NavBar />
+        <Home />
     </>
   )
 }
---
+++ src/Sections/Home.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (  
+    <section id="home" className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+
+    </section>
+    )
+
+}
